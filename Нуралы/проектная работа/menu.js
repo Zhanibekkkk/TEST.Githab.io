5f3016b7037bf142776b6ecea9607da4ad71b993
Zhanibekkkk <118690598+Zhanibekkkk@users.noreply.github.com>
2022-11-25 22:15:24 +0600

    Add files via upload

---
+++ Нуралы/проектная работа/menu.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.menu-burger__header').click(function() {
+        $('.menu-burger__header').toggleClass('open-menu');
+    });
+});
+
+$(document).ready(function() {
+    $('.menu-burger__header').click(function(){
+        $('.menu-burger__header').toggleClass('open-menu');
+        $('.header__nav').toggleClass('open-menu');
+    });
+});
